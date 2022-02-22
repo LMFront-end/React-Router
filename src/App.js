208e2f30606607910cf62e16d859b6d0cb8697bc
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-21 23:06:39 -0500

    se crean componentes para inicio, nosotros y contacto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div className="container">
+          <h1>Navbar...</h1>
+          <hr />
+          <Switch>
+              <Route path="/contacto">
+                    Esta es la pagina de contacto
+              </Route>
+              <Route path="/nosotros">
+                    Esta es la pagina de nosotros
+              </Route>
+              <Route path="/">
+                    Esta es la pagina de incio
+              </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
